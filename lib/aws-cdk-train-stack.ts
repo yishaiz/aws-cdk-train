5d71dee7aee9a479c214a6306822de5e6792c392
yishai <yishai5555@gmail.com>
2026-01-21 22:27:45 +0200

    03-outputs

--- lib/aws-cdk-train-stack.ts
+++ lib/aws-cdk-train-stack.ts
@@ -1,16 +1,48 @@
 import * as cdk from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AwsCdkTrainStack extends cdk.Stack {
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id);
+
+    new Bucket(this, 'L3Bucket', {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(expiration),
+        },
+      ],
+    });
+  }
+}
+
+export class CdkStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create an s3 bucket 3 ways:
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [
+          {
+            expirationInDays: 1,
+            status: 'Enabled',
+          },
+        ],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkTrainQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myBucket = new Bucket(this, 'MyL2Bucket', {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(2),
+        },
+      ],
+    });
+
+    console.log(`L2 Bucket Name: ${myBucket.bucketName}`);
+
+    new L3Bucket(this, 'MyL3Bucket', 3);
   }
 }
