d90ba1829caa069a4de840f18b35c64d65cda275
yishai <yishai5555@gmail.com>
2026-03-01 16:28:57 +0200

    04-add-multiple-stacks

--- bin/aws-cdk-train.ts
+++ bin/aws-cdk-train.ts
@@ -1,6 +1,8 @@
-#!/usr/bin/env node
+import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStarterStack } from '../lib/aws-cdk-train-stack';
+import { PhotosStack } from '../lib/PhotosStack';
+import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
 
 const app = new cdk.App();
-new CdkStarterStack(app, 'AwsCdkTrainStack', {});
+new PhotosStack(app, 'PhotosStack');
+new PhotosHandlerStack(app, 'PhotosHandlerStack');
---
+++ lib/PhotosStack.ts
@@ -0,0 +1,28 @@
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
+import { CfnOutput, Fn } from 'aws-cdk-lib';
+
+export class PhotosStack extends cdk.Stack {
+  private stackSuffix: string;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.initializeSuffix();
+
+    const photosBucket = new Bucket(this, 'PhotosBucket2', {
+      bucketName: `photos-bucket-${this.stackSuffix}`,
+    });
+
+    new CfnOutput(this, 'photos-bucket', {
+      value: photosBucket.bucketArn,
+      exportName: 'photos-bucket',
+    });
+  }
+
+  private initializeSuffix() {
+    const shortStackId = Fn.select(2, Fn.split('/', this.stackId));
+    this.stackSuffix = Fn.select(4, Fn.split('-', shortStackId));
+  }
+}
